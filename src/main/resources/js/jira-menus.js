8a0f64f9b17ec8396163c1ef589939c6c32545bb
Andrii Maliuta <quadr988@gmail.com>
2023-02-02 20:58:23 +0200

    adding prop section in user menu when hovering mouse over user mentions

--- src/main/resources/js/jira-menus.js
+++ src/main/resources/js/jira-menus.js
@@ -1,24 +1,26 @@
-AJS.toInit(function () {
-    setPropInDialog();
+AJS.toInit(async function () {
+    await setPropInDialog();
 })
 
 async function setPropInDialog() {
     document.querySelectorAll('a.user-hover').forEach(userMention => {
         const currentUser = document.querySelector("meta[name='ajs-remote-user']").content;
-        userMention.addEventListener('mouseover', async evt => {
-            console.log(">>> inside :: addEventListener");
-            const resp = await fetch(`/rest/api/2/user/properties/department?username=${currentUser}`)
-            const res = await resp.json();
+            userMention.addEventListener('mouseover', evt => {
+                (async () => {
+                    console.log(">>> inside :: addEventListener");
+                    const resp = await fetch(`/rest/api/2/user/properties/department?username=${currentUser}`)
+                    const res = await resp.json();
 
-            const userPropKey = res.key;
-            const userPropValue = res.value.department;
-            let newProp = document.createElement('div');
-            newProp.innerHTML = `<div>
+                    const userPropKey = res.key;
+                    const userPropValue = res.value.department;
+                    let newProp = document.createElement('div');
+                    newProp.innerHTML = `<div>
                             <p>Department: ${userPropValue}</p>
                         </div>`;
-            console.log(userMention)
-            console.log(userMention.querySelector('.user-hover-details'))
-            userMention.querySelector('.user-hover-details').parentElement.appendChild(newProp);
-        });
+                    console.log(userMention)
+                    console.log(userMention.querySelector('.user-hover-details'))
+                    userMention.querySelector('.user-hover-details').parentElement.appendChild(newProp);
+                })()
+            });
     });
 }
